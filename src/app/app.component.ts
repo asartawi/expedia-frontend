841e75bc716874d7653fabe7bec56d94be1e3e4e
abdulhafeth <abd.sarta@gmail.com>
2018-02-13 22:40:09 +0200

    integrate with google places

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,77 +1,149 @@
-import { Component } from '@angular/core';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component} from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
-  name: 'rateOrdinal',
-  pure: false
+    name: 'rateOrdinal',
+    pure: false
 })
 
 export class rateOrdinalPipe implements PipeTransform {
-  transform(items: Object, filter: Object): any {
-    if (!items && !filter) {
-      return items;
-    }
+    transform(items: Object, filter: Object): any {
+        if (!items && !filter) {
+            return items;
+        }
 
-    // filter items array, items which match and return true will be
-    // kept, false will be filtered out
-    if (items >= 4.7){
-      return "Exceptional!";
-    }
-    if (items >= 4.5){
-      return "Wonderful!";
-    }
+        // filter items array, items which match and return true will be
+        // kept, false will be filtered out
+        if (items >= 4.7) {
+            return "Exceptional!";
+        }
+        if (items >= 4.5) {
+            return "Wonderful!";
+        }
 
-    if (items >= 4.3){
-      return "Excellent!";
-    }
+        if (items >= 4.3) {
+            return "Excellent!";
+        }
 
-    if (items >= 4){
-      return "Very Good!";
-    }
+        if (items >= 4) {
+            return "Very Good!";
+        }
 
-    if (items >= 3.5){
-      return "Good!";
-    }
+        if (items >= 3.5) {
+            return "Good!";
+        }
 
-    return "";
-  }
+        return "";
+    }
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
 
 @Injectable()
 export class AppComponent {
-  offers = []
-  response = {
-    offers: []
-  };
+    offers = [];
+    destination = {};
+    guestRating = null;
+    starRating = null;
+    priceOptions = null;
+    loading = true;
+    startDate = new Date();
+    endDate = new Date(this.startDate.getTime() + (1000 * 60 * 60 * 24));
 
-  constructor(private http: HttpClient) {
-    this.http.get("https://stark-eyrie-83089.herokuapp.com/hotels/deals")
-        .subscribe(data => {
-          this.offers = data["offers"]["Hotel"]
-        },
-            err => this.logError(err),
-        );
-  };
+    constructor(private http: HttpClient) {
+        function initialize() {
+            var input = document.getElementById('searchTextField');
+            new google.maps.places.Autocomplete(input);
+        }
 
-  round = function(num){
-    return Math.round(num);
-};
+        google.maps.event.addDomListener(window, 'load', initialize);
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
 
-  title = 'app';
+                console.log(position)
+            }, function() {
+                console.log("error")
+            });
+        }
+        this.getHotelsDeals();
+    };
 
-  logError(err: string) {
-    console.error('There was an error: ' + err);
-  }
+    round = function (num) {
+        return Math.round(num);
+    };
+
+    getHotelsDeals = function () {
+        let params = new HttpParams();
+        if (this.location) {
+            params = params.append('destinationName', this.location);
+        }
+
+        if (this.guestRating) {
+            params = params.append('minGuestRating', this.guestRating);
+        }
+
+        if (this.starRating) {
+            params = params.append('minStarRating', this.starRating);
+        }
+        if (this.priceOptions) {
+            switch(this.priceOptions){
+                case 1:
+                    params = params.append('maxAverageRate', "74");
+                    break;
+                case  2:
+                    params = params.append('minAverageRate', "75");
+                    params = params.append('maxAverageRate', "124");
+                    break;
+                case  3:
+                    params = params.append('minAverageRate', "125");
+                    params = params.append('maxAverageRate', "199");
+                    break;
+                case  4:
+                    params = params.append('minAverageRate', "200");
+                    params = params.append('maxAverageRate', "299");
+                    break;
+                case  5:
+                    params = params.append('minAverageRate', "300");
+                    break;
+            }
+        }
+
+        this.loading = true;
+        this.http.get("https://stark-eyrie-83089.herokuapp.com/hotels/deals", {params: params})
+            .subscribe(data => {
+                    this.offers = data["offers"]["Hotel"];
+                    this.destination = data["offers"]["Hotel"][0]["destination"];
+                    this.loading = false;
+                },
+                err => this.logError(err),
+            );
+    };
+
+    onRatingChange = function () {
+        this.getHotelsDeals();
+    };
+
+    replcaeImageLink = function (link) {
+        return link.replace("_t", "_y");
+
+    }
+
+    logError(err: string) {
+        console.error('There was an error: ' + err);
+    }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
 import {rateOrdinalPipe} from './app.component'
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -11,8 +13,13 @@ import {rateOrdinalPipe} from './app.component'
     rateOrdinalPipe
   ],
   imports: [
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyDu9o67T-7gIkQ1N_p9WoLtfjjA1FKJPno",
+      libraries: ["places"]
+    }),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
